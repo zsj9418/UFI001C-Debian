10965ce6a903dc0a75577c0f135305496176d762
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-08-04 17:15:00 +0800

    Update chroot.sh

--- rootfs/chroot.sh
+++ rootfs/chroot.sh
@@ -6,17 +6,20 @@ NAME=UFI001C
 PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e
 
 cat <<EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+##deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
 # deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+##deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
 # deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+##deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
 # deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
 
-deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+##deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
 # deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ sid main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ usid main contrib non-free non-free-firmware
 EOF
 
 cat <<EOF > /etc/fstab

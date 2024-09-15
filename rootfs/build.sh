c3ae8fc9c4ffcc8ae4fbfd7d56c844bfbadd4d82
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-09-16 03:08:46 +0800

    Update build.sh

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
 
-DIST=bookworm
-BOOT_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot.img"
-BOOT_NO_MODEM_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot-no-modem.img"
-BOOT_NO_MODEM_OC_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot-no-modem-oc.img"
+DIST=trixie
+BOOT_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-2/boot.img"
+BOOT_NO_MODEM_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-2/boot-no-modem.img"
+BOOT_NO_MODEM_OC_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-2/boot-no-modem-oc.img"
 K_IMAGE_DEB_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/linux-image-6.6.43-g5147e6199c4f_6.6.43-g5147e6199c4f-1_arm64.deb"
-K_DEV_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/tag/6.6.43-1"
+K_DEV_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/tag/6.6.43-2"
 UUID=62ae670d-01b7-4c7d-8e72-60bcd00410b7
 
 if [ `id -u` -ne 0 ]

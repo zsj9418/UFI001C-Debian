aa266415f07995f21af1a084031bc7afe7e87929
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-09-15 23:23:24 +0800

    Update build.sh

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 
-DIST=trixie
+DIST=bookworm
 BOOT_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot.img"
 BOOT_NO_MODEM_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot-no-modem.img"
 BOOT_NO_MODEM_OC_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/6.6.43-1/boot-no-modem-oc.img"

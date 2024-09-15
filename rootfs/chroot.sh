2b39319742ddf1b16d927e1d589bd5044d713b4f
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-09-16 03:09:12 +0800

    Update chroot.sh

--- rootfs/chroot.sh
+++ rootfs/chroot.sh
@@ -9,17 +9,17 @@ WIFIPASS=00000000
 PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e
 
 cat <<EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
-# deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ trixie-backports main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ trixie-backports main contrib non-free non-free-firmware
 
-deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
-# deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+# deb-src http://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
 EOF
 
 cat <<EOF > /etc/fstab

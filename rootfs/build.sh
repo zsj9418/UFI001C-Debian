34219f2169edbcad98cc0e0dd068d523ada17a11
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-09-17 02:23:35 +0800

    Update build.sh

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -2,11 +2,11 @@
 
 
 DIST=trixie
-BOOT_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/untagged-40bc09934d2010e405c7/boot.img"
-BOOT_NO_MODEM_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/untagged-40bc09934d2010e405c7/boot-no-modem.img"
-BOOT_NO_MODEM_OC_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/untagged-40bc09934d2010e405c7/boot-no-modem-oc.img"
-K_IMAGE_DEB_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/download/untagged-40bc09934d2010e405c7/linux-image-6.6.48-g49bebcb4bf85_6.6.48-g49bebcb4bf85-1_arm64.deb"
-K_DEV_URL="https://github.com/YANXIAOXIH/UFI001C-kernel/releases/tag/untagged-40bc09934d2010e405c7"
+BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot.img"
+BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot-no-modem.img"
+BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot-no-modem-oc.img"
+K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/linux-image-6.6.49-msm8916-gba356d6a3331_6.6.49-gba356d6a3331-1_arm64.deb"
+K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.6.49-1"
 UUID=62ae670d-01b7-4c7d-8e72-60bcd00410b7
 
 if [ `id -u` -ne 0 ]

c0332c4624025abc9b201235a5fa5b05836fe7fd
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2025-01-06 22:52:32 +0800

    Update build.sh

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -2,11 +2,11 @@
 
 
 DIST=trixie
-BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot.img"
-BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot-no-modem.img"
-BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/boot-no-modem-oc.img"
-K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.49-1/linux-image-6.6.49-msm8916-gba356d6a3331_6.6.49-gba356d6a3331-1_arm64.deb"
-K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.6.49-1"
+BOOT_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot.img"
+BOOT_NO_MODEM_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot-no-modem.img"
+BOOT_NO_MODEM_OC_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/boot-no-modem-oc.img"
+K_IMAGE_DEB_URL="https://github.com/KyonLi/ufi003-kernel/releases/download/6.6.60-1/linux-image-6.6.60-msm8916-g32b5a619bbf5_6.6.60-g32b5a619bbf5-1_arm64.deb"
+K_DEV_URL="https://github.com/KyonLi/ufi003-kernel/releases/tag/6.6.60-1"
 UUID=62ae670d-01b7-4c7d-8e72-60bcd00410b7
 
 if [ `id -u` -ne 0 ]
